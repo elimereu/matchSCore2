5b373f9b9ebba8ab47253ece38a2d299054505b5
Federico Marini <marinif@uni-mainz.de>
2019-12-11 21:49:17 +0100

    Added help for package - will also be a good place to store imports and handling namespace

---
+++ R/matchSCore2-pkg.R
@@ -0,0 +1,9 @@
+#' matchSCore2
+#'
+#' `matchSCore2` is a Bioconductor package that provides ...
+#'
+#' @importFrom stats cor median na.omit predict
+#'
+#' @name matchSCore2-pkg
+#' @docType package
+NULL
