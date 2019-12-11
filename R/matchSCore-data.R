4ea94ca37efacc8551681a9467a6530d0ccd4cdd
Federico Marini <marinif@uni-mainz.de>
2019-12-11 21:55:02 +0100

    placeholder for documenting the datasets included

---
+++ R/matchSCore-data.R
@@ -0,0 +1,17 @@
+#' gene_cl
+#'
+#' @name gene_cl
+#' @docType data
+NULL
+
+#' gene_cl.obs
+#'
+#' @name gene_cl.obs
+#' @docType data
+NULL
+
+#' gene_cl.ref
+#'
+#' @name gene_cl.ref
+#' @docType data
+NULL
