3d28a734b71849160ba0e8b3cd801a66a2ee3279
Federico Marini <marinif@uni-mainz.de>
2019-12-12 17:41:13 +0100

    linting... cont'd

--- R/summary_ggplot.R
+++ R/summary_ggplot.R
@@ -1,20 +1,30 @@
 # TODO: to be exported - or anyway documented?
-summary_ggplot <- function(data,ylab,xlab){
+summary_ggplot <- function(data,
+                           ylab,
+                           xlab) {
 
   # library(ggplot2)
   # library(reshape2) # TODO: really needed?
   # library(grid) # TODO: really needed?
 
-  my_df <- data.frame(t(data),check.names = FALSE,check.rows = FALSE)
-  my_df.melt <-  melt(cbind(x=1:nrow(my_df),my_df),id ="x")
+  my_df <- data.frame(t(data), check.names = FALSE, check.rows = FALSE)
+  my_df.melt <-  melt(cbind(x = 1:nrow(my_df), my_df),
+                      id = "x")
 
-  gg <- ggplot(my_df.melt, aes(x=factor(x),y=variable,fill=value)) + labs(x=xlab,y=ylab)+
-    geom_tile(aes(fill = value)) + scale_x_discrete(lab=rownames(my_df))+
-    theme(axis.text.x=element_text(angle=30,hjust = 1,size=16),axis.text.y = element_text(size=16),axis.title = element_text(size=16))+
+  gg <- ggplot(my_df.melt,
+               aes(x = factor(x),
+                   y = variable,
+                   fill = value)) +
+    labs(x = xlab,
+         y = ylab) +
+    geom_tile(
+      aes(fill = value)) +
+    scale_x_discrete(lab = rownames(my_df)) +
+    theme(
+      axis.text.x = element_text(angle = 30, hjust = 1, size = 16),
+      axis.text.y = element_text(size = 16), axis.title = element_text(size = 16)) +
     geom_text(aes(label = round(value, 2))) +
-    scale_fill_gradient(low = "white", high = "red",name="Jaccard Index \n")
-
+    scale_fill_gradient(low = "white", high = "red", name = "Jaccard Index \n")
 
   return(gg)
 }
-
