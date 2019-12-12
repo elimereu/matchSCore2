476109f130bc4683dfbe4b5f39489f6b959195d5
Federico Marini <marinif@uni-mainz.de>
2019-12-12 16:13:59 +0100

    Added some more imports

--- R/matchSCore2-pkg.R
+++ R/matchSCore2-pkg.R
@@ -2,15 +2,27 @@
 #'
 #' `matchSCore2` is a Bioconductor package that provides ...
 #'
-#' @importFrom stats cor median na.omit predict
 #' @importFrom graphics plot
 #' @importFrom grDevices colorRampPalette rainbow
 #' @importFrom methods new
 #' @importFrom stats cor median na.omit predict
 #' @importFrom utils setTxtProgressBar txtProgressBar
-#' @importFrom Matrix colSums
-#' @importFrom topGO GenTable getSigGroups GOKSTest printGraph
+#' @import Matrix
+#' @importFrom topGO GenTable getSigGroups GOKSTest printGraph annFUN.org
+#' annFUN.GO2genes score
+#' @import ggplot2
+#' @import org.Hs.eg.db
+#' @import org.Mm.eg.db
+#' @importFrom reshape2 melt
+#' @importFrom grid unit
+#' @importFrom corrplot corrplot
+#' @importFrom nnet multinom
+#' @import igraph
+#' @import cowplot
+#' @import Seurat
 #'
 #' @name matchSCore2-pkg
 #' @docType package
 NULL
+
+# cowplot although here we use ggarrange, TODO? (is it from ggpubr)
