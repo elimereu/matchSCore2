15b4c097efb3a87836436d6e79f528e68e10ff46
Federico Marini <marinif@uni-mainz.de>
2019-12-12 15:41:01 +0100

    Added some required imports

--- R/matchSCore2-pkg.R
+++ R/matchSCore2-pkg.R
@@ -3,6 +3,13 @@
 #' `matchSCore2` is a Bioconductor package that provides ...
 #'
 #' @importFrom stats cor median na.omit predict
+#' @importFrom graphics plot
+#' @importFrom grDevices colorRampPalette rainbow
+#' @importFrom methods new
+#' @importFrom stats cor median na.omit predict
+#' @importFrom utils setTxtProgressBar txtProgressBar
+#' @importFrom Matrix colSums
+#' @importFrom topGO GenTable getSigGroups GOKSTest printGraph
 #'
 #' @name matchSCore2-pkg
 #' @docType package
