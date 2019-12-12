f239482714bad540f2e1c7d939894aefab57f41d
Federico Marini <marinif@uni-mainz.de>
2019-12-12 21:24:53 +0100

    seurat3_run in tool_scores_...

--- R/tool_scores_by_specificity.R
+++ R/tool_scores_by_specificity.R
@@ -5,10 +5,10 @@
 #' @param sim A Splatter simulation object.
 #' @param specificity The proportion of top-ranked markers for each simulated group.
 #' @param tool_out The output of the used tool to cluster your data. It should be
-#' as the output of the `seurat_run` function.
+#' as the output of the `seurat3_run` function.
 #' @param ntop The proportion of top-ranked markers for each cluster.
 #' @param tool_run The name of the function used to run the clusters without quote.
-#' Default=`seurat_run`).
+#' Default=`seurat3_run`).
 #' @param labels Cluster labels as in the output of the [compute_labels()] function.
 #'
 #' @return A `matchSCore` value
@@ -17,7 +17,12 @@
 #'
 #' @examples
 #' # TODO
-tool_scores_by_specificity = function(sim,specificity,tool_out,ntop,tool_run=seurat_run,labels){
+tool_scores_by_specificity <- function(sim,
+                                       specificity,
+                                       tool_out,
+                                       ntop,
+                                       tool_run = seurat3_run,
+                                       labels) {
 
   fd=rowData(sim)
   pd=colData(sim)
