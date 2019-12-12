6ae2b6e7d376228b05a8f7867142cadf01d0069c
Federico Marini <marinif@uni-mainz.de>
2019-12-12 17:32:25 +0100

    linting... cont'd

--- R/compute_labels.R
+++ R/compute_labels.R
@@ -12,21 +12,24 @@
 #'
 #' @examples
 #' # TODO
-compute_labels=function(idc,idc1){
+compute_labels <- function(idc,
+                           idc1) {
 
-  idc=factor(idc)
-  idc1=factor(idc1)
-  lev=levels(idc)
-  lev1=levels(idc1)
+  idc <- factor(idc)
+  idc1 <- factor(idc1)
+  lev <- levels(idc)
+  lev1 <- levels(idc1)
 
-for(i in 1:length(lev)){
-  p1=which(idc==i)
-  cl=as.numeric(idc1[p1])
-  x=as.numeric(names(sort(table(cl),decreasing=TRUE)[1]))
-  if(i==1){lab=x} else{lab=append(lab,values = x,after = length(lab))}
-}
+  for (i in 1:length(lev)) {
+    p1 <- which(idc == i)
+    cl <- as.numeric(idc1[p1])
+    x <- as.numeric(names(sort(table(cl), decreasing = TRUE)[1]))
+    if (i == 1) {
+      lab <- x
+    } else {
+      lab <- append(lab, values = x, after = length(lab))
+    }
+  }
 
   return(lab)
 }
-
-
