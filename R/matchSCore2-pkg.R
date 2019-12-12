b1b945e107aa9fa0b07843591accef3c19bd5ef8
Federico Marini <marinif@uni-mainz.de>
2019-12-12 16:13:59 +0100

    solved conflicts

--- R/matchSCore2-pkg.R
+++ R/matchSCore2-pkg.R
@@ -2,8 +2,27 @@
 #'
 #' `matchSCore2` is a Bioconductor package that provides ...
 #'
+#' @importFrom graphics plot
+#' @importFrom grDevices colorRampPalette rainbow
+#' @importFrom methods new
 #' @importFrom stats cor median na.omit predict
+#' @importFrom utils setTxtProgressBar txtProgressBar
+#' @import Matrix
+#' @importFrom topGO GenTable getSigGroups GOKSTest printGraph annFUN.org
+#' annFUN.GO2genes score
+#' @import ggplot2
+#' @import org.Hs.eg.db
+#' @import org.Mm.eg.db
+#' @importFrom reshape2 melt
+#' @importFrom grid unit
+#' @importFrom corrplot corrplot
+#' @importFrom nnet multinom
+#' @import igraph
+#' @import cowplot
+#' @import Seurat
 #'
 #' @name matchSCore2-pkg
 #' @docType package
 NULL
+
+# cowplot although here we use ggarrange, TODO? (is it from ggpubr)
