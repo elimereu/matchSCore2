bcf8d020773b37cbcc5d5d64841e5b31b67f6689
elimereu <mereu.elisabetta@gmail.com>
2020-03-18 16:41:35 +0100

    align_run and vignette

--- R/align_run.R
+++ R/align_run.R
@@ -1,20 +1,33 @@
-#' Integration of different datasets 
+#' Integration of different datasets
 #'
-#' @param dataset_list A named list of annotated Seurat objects. The cell annotations are in the @active.ident slots.
-#' @param marker_list A list of cell-type specific markers from the input dataset. 
-#' @return A list containing: 
-#' \item integrated: the integrated scaled matrix.
-#' \item counts: the combined count matrix.
-#' \item annotation_label: the cell annotations.
-#' \item dataset_label: a vector indicating the dataset of origin for each cell.
-#' \item genes: the set of common genes used.
+#' @param dataset_list A named list of annotated SingleCellExperiments objects. The cell
+#' annotations are in the `cluster` slots.
+#' @param marker_list A list of `cluster` specific markers from the input dataset.
+#' @param ref The name of the reference dataset from the dataset_list. It has to be the first in the dataset_list.
+#'
+#' @return A list containing:
+#' - `sce`: the integrated SingleCellExperiment object.
+#' - `integrated`: the integrated scaled matrix.
+#' - `counts`: the combined count matrix.
+#' - `annotation_label`: the cell annotations.
+#' - `dataset_label`: a vector indicating the dataset of origin for each cell.
+#' - `genes`: the set of common genes used.
+#'
 #' @export
-#' @examples 
-#' 
-
-align_run <- function(dataset_list,marker_list){
-  require(Seurat)
-  require(Matrix)
+#'
+#' @examples
+#' # TODO
+align_run <- function(dataset_list,marker_list,ref){
+  
+  if(names(dataset_list)[1]!=ref){
+    stop("The reference dataset is not the first of the dataset list")
+  }
+  
+  if(ncol(dataset_list[[ref]])>2000){
+       prop <- round(2000/(ncol(dataset_list[[ref]])),digits = 2)
+  }else{ prop <- 0.9}
+  
+  original <- dataset_list
   print("Defining the set of common genes")
   
   total <- 10
@@ -28,40 +41,91 @@ align_run <- function(dataset_list,marker_list){
   
   start.time <- Sys.time()
   
-    
-    genes <-unique(unlist(marker_list))
-    
-    bg <- lapply(dataset_list,function(x) intersect(rownames(x),genes))
-    l <- length(bg)
-    t <- table(unlist(bg))
-    genes <- names(t)[which(t==l)]
+  
+  genes <-unique(unlist(marker_list))
+  
+  bg <- lapply(dataset_list,function(x) intersect(rownames(x),genes))
+  l <- length(bg)
+  t <- table(unlist(bg))
+  genes <- names(t)[which(t==l)]
   
   progress <- 2
   Sys.sleep(0.1)
   setTxtProgressBar(pb,progress)
   
+  ref.p <- which(names(dataset_list)==ref)
+  ref.cl <- factor(dataset_list[[ref]]@colData$cluster)
+  sizes <- unlist(lapply(levels(ref.cl),function(x) length(ref.cl[which(ref.cl %in% x)])))
+  names(sizes) <- levels(ref.cl)
   
   
-  dataset_list <- lapply(dataset_list,function(x) ScaleData(x,features = genes,verbose = F))
-  d_list <- lapply(dataset_list,function(x) x@assays$RNA@scale.data[genes,])  
-  cl <- unique(unlist(lapply(dataset_list,function(x) factor(x@active.ident))))
+  train.sample <- unlist(sapply(levels(ref.cl),function(x) sample(x = which(ref.cl %in% x), size = sizes[names(sizes) == x] * prop)))
+  s <- colnames(dataset_list[[ref]])[train.sample]
+  train.data <- dataset_list[[ref]]@assays$data$logcount[, train.sample]
+  
+  test.sample <- which(!colnames(dataset_list[[ref]]) %in% s)
+  test.data <- dataset_list[[ref]]@assays$data$logcount[, test.sample]
+  
+  len <- length(dataset_list)
+  dataset_list$ref <- dataset_list[[ref]][,train.sample]   
+  dataset_list$test <- dataset_list[[ref]][,test.sample]   
+  dataset_list <- dataset_list[-ref.p]
+  
+  
+  d_list <- lapply(dataset_list,function(x) x@assays$data$logcount[genes,])
+  #d_list <- lapply(d_list,function(x) t(scale(t(x),center = T,scale = apply(x, 1, sd, na.rm = TRUE))))
+  d_list <- lapply(d_list,function(x) (x-min(x))/(max(x)-min(x)))
+  cl <- lapply(dataset_list,function(x) factor(x@colData$cluster))
   
   progress <- 3
   Sys.sleep(0.1)
   setTxtProgressBar(pb,progress)
   
-
-  cc <- lapply(dataset_list,function(x) sapply(levels(cl),function(y) rowMeans(x@assays$RNA@scale.data[genes,which(x@active.ident==y)]) ))
+  
+  to_rm <- lapply(dataset_list,function(x) names(table(x@colData$cluster))[which(as.vector(table(x@colData$cluster))<10)])
+  
+  # if(length(to_rm)>1){
+  #     print(paste(" ",to_rm,"cluster has limitated size"))
+  #     stop("Try to combine them with other clusters")
+  # }
+  # # 
+  # cl1 <- levels(factor(unlist(cl)))
+  # cl1 <- cl1[which(!(cl1 %in% to_rm))]
+  # levels(cl[[x]])[!which(cl[[x]] %in% to_rm[[x]])]
+  # cl1 <- cl[[1]]
+  # cl1[which(cl1 %in% to_rm)] <- NA
+  # cl1 <- factor(cl1)
+  # 
+  # cl1 <- lapply(c(1:length(to_rm)),function(x) cl[[x]][which(cl[[x]] %in% to_rm[[x]])])
+  # 
+  cl1 <- cl
+  for(i in c(1:length(to_rm))){
+    cl1[[i]][which(cl1[[i]] %in% to_rm[[i]])] <- NA
+    cl1[[i]] <- factor(cl1[[i]])
+  }
+  
+  
+  # sapply(levels(cl[[x]])[which(to_rm[[x]] %in% levels(cl[[x]]))],function(y) print(y))
+  # lapply(seq(1:length(d_list)),function(x) sapply(levels(cl[[x]])[which(to_rm[[x]] %in% levels(cl[[x]]))],function(y) print(y) ))
+  cc <- lapply(seq(1:length(d_list)),function(x) sapply(levels(cl1[[x]]),function(y) rowMeans(d_list[[x]][genes,which(dataset_list[[x]]@colData$cluster==y)]) ))
   cc <- lapply(cc,function(x) apply(x,1,function(y) median(y,na.rm = T)))
   
   progress <- 4
   Sys.sleep(0.1)
   setTxtProgressBar(pb,progress)
   
+  require(corpcor)
   
-  H <- lapply(seq(1,l),function(x) d_list[[x]]-cc[[x]])
-  cov <- lapply(seq(2,l), function(x) cov(H[[1]],H[[x]]))
-  svd_out <- lapply(cov, function(x) svd(x))
+  len <- length(dataset_list)
+  sequence <- c(1:len)
+  pos <- which(names(dataset_list)=="ref")
+  sequence <- sequence[-pos]
+  
+  H <- lapply(c(1:len),function(x) d_list[[x]]-cc[[x]])
+  print(" Computing covariance matrixes... ")
+  cov <- lapply(sequence, function(x) cov(H[[pos]],H[[x]]))
+  print(" Single Value Decomposition of covariance matrixes ... ")
+  svd_out <- lapply(cov, function(x) fast.svd(x))
   
   progress <- 5
   Sys.sleep(0.1)
@@ -69,26 +133,24 @@ align_run <- function(dataset_list,marker_list){
   
   
   Ropt <- lapply(svd_out, function(x) x$v %*% t(x$u)) 
-  new_coord <- lapply(Ropt,function(x) x %*% t(H[[1]]))
+  new_coord <- lapply(Ropt,function(x) x %*% t(H[[pos]]))
   
   
   progress <- 6
   Sys.sleep(0.1)
   setTxtProgressBar(pb,progress)
   
-  for(i in seq(1,l)){
-    if(i>1){
-      integrated <- cbind(integrated,t(new_coord[[i-1]]))
-    }else{
-      integrated <- H[[1]]
-    }
-    
+  integrated <- cbind(H[[pos]],t(new_coord[[pos]]))
+  
+  for(i in 1:(length(new_coord)-1)){
+    integrated <- cbind(integrated,t(new_coord[[i]]))
   }
   
-  cells <- unlist(lapply(dataset_list,function(x) names(x@active.ident)))
+  
+  cells <- unlist(lapply(original,function(x) colnames(x)))
   colnames(integrated) <- cells
   
-  counts_list <- lapply(dataset_list,function(x) x@assays$RNA@counts[genes,names(x@active.ident)])  
+  counts_list <- lapply(original,function(x) x@assays$data$counts[genes,colnames(x)])  
   #md <- lapply(dataset_list,function(x) x@meta.data[names(x@active.ident),])  
   
   progress <- 7
@@ -97,37 +159,25 @@ align_run <- function(dataset_list,marker_list){
   
   
   for(i in seq(1,l)){
-     if(i>1){
-       counts <- cbind(counts,counts_list[[i]])
-     }else{
-       counts <- counts_list[[1]] 
-     }}
+    if(i>1){
+      counts <- cbind(counts,counts_list[[i]])
+    }else{
+      counts <- counts_list[[1]] 
+    }}
   
   progress <- 8
   Sys.sleep(0.1)
   setTxtProgressBar(pb,progress)
   
   
-  #require(plyr)
   
-  # for(i in seq(1,l)){
-  # 
-  #   if(i>1){
-  #     meta.data <- rbind.fill(meta.data,md[[i]])
-  #   }else{
-  #     meta.data <- md[[1]]
-  #   }}
-    
-    
-    # meta.data$clus_annotation <- unlist(lapply(dataset_list,function(x) factor(x@active.ident)))
-    # rownames(meta.data) <- colnames(integrated)
-  annotation <- unlist(lapply(dataset_list,function(x) as.character(x@active.ident)))
+  annotation <- unlist(lapply(original,function(x) x@colData$cluster))
   
   progress <- 9
   Sys.sleep(0.1)
   setTxtProgressBar(pb,progress)
   
-  dataset <- unlist(lapply(seq(1:length(dataset_list)),function(x) rep(names(dataset_list)[x],ncol(dataset_list[[x]]))))
+  dataset <- unlist(lapply(seq(1:length(original)),function(x) rep(names(original)[x],ncol(original[[x]]))))
   
   progress <- 10
   Sys.sleep(0.1)
@@ -137,6 +187,14 @@ align_run <- function(dataset_list,marker_list){
   time <- difftime(end.time,start.time,units="mins")
   print(paste("The runtime is:",time,"min",sep=" "))
   
-  return(list(counts=counts,integrated=integrated,annotation_label=annotation,dataset_label=dataset,genes=genes))
+  require(SingleCellExperiment)
+  sce <- SingleCellExperiment(assays=list(counts=counts))
+  minx <- 0
+  maxx <- max(as.vector(log10(counts+1)),na.rm = T)
+  integrated <- t(apply(integrated,1,function(x) (x-min(x))/(max(x)-min(x))))
+  sce@assays$data$integrated <- integrated
+  sce@colData <- DataFrame(cluster=annotation,batch=dataset)
+  colnames(sce) <- cells
+  return(list(sce=sce,counts=counts,integrated=integrated,annotation_label=annotation,dataset_label=dataset,genes=genes))
   
 }
---
+++ R/identity_heatmap.R
@@ -0,0 +1,36 @@
+#' Heatmaps of class probabilities
+#'
+#' This function identifies true label groups between reference groups and clusters.
+#' @param out output of the function identity_map.
+#' @return The heatmap displaying the probability of each cell to belong to the identity classes according the model.
+#'
+#' @export
+#' @examples
+#'
+
+identity_heatmap <- function(out){
+library(ggplot2)
+library(reshape2)
+library(grid)
+
+
+ids.ord <- order(out$ids)
+ord.p <- out$fit.prob[ids.ord,]
+
+my_df <- data.frame(ord.p,check.names = F,check.rows = F)
+my_df.melt <-  melt(cbind(x=1:nrow(my_df),my_df),id ="x")
+
+
+gg <-  ggplot(my_df.melt, aes(x=factor(x),y=variable,fill=value)) + labs(x="Cells",y="Cell identity")+
+  geom_tile(aes(fill = value)) + scale_fill_gradientn(colours = c("gray100","blue","yellow","deeppink"),
+                                                      name="Probability\n")+
+   theme(axis.text.x=element_blank(),
+        axis.text.y = element_text(colour = "black",size=16),axis.title = element_text(size=16),
+        legend.text = element_text(size=12),legend.title = element_text(size = 12))
+
+return(gg)
+}
+
+png(file="vignettes/identity_heatmaps.png",width = 7, height = 6, units = 'in', res = 600)
+gg
+dev.off()
