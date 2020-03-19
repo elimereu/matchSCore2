385d6797da5f9848ee386f51d4ade6b87cd06f20
elimereu <mereu.elisabetta@gmail.com>
2020-03-19 15:19:38 +0100

    add seurat3_run

--- R/seurat3_run.R
+++ R/seurat3_run.R
@@ -1,51 +1,54 @@
 #' This function run Seurat3 by using the output of the align_run function.
 #'
-#' @param counts The output of the function align_run. The combined count matrix used to create the new integrated Seurat object. 
-#' @param integrated The output of the function align_run. It will be used as the scale.data of the new integrated Seurat object. 
-#' @param annotation A vector with the cell annotation like the annotation_label output of the align_run function.  
-#' @param dataset a vector indicating the dataset of origin for each cell like the dataset_label output of the align_run function.
-#' @param dims Seurat parameter. It is the dimension of the PCA space. 
-#' @param res Seurat resolution parameter.  
-#' @return The integrated Seurat object. Two UMAP plots with cells coloured by annotation and dataset will be generated. 
+#' @param out_align The output of the function align_run. The combined count matrix is used
+#' to create the Seurat object and the integrated is provided to the slot @data.
+#' @param dims Seurat parameter. It is the dimension of the PCA space.
+#' @param res Seurat resolution parameter.
+#' @return The integrated Seurat object. Two UMAP plots with cells coloured by annotation and dataset will be generated.
 #' @export
-#' @examples 
-#' 
+#' @examples
+#'
 
 
-seurat3_run <- function(counts,integrated,annotation,dataset,dims=c(1:10),res=0.2,col_anno=NULL,col_data=NULL){
-  
+seurat3_run <- function(out_align,dims=c(1:10),res=0.2,col_anno=NULL,col_data=NULL){
+
   require(Seurat)
   require(cowplot)
   require(ggplot2)
-  
+
+  counts <- out_align$counts
+  integrated <- out_align$integrated
+  annotation <- out_align$annotation_label
+  dataset <- out_align$dataset_label
+
   print("Running Seurat by using as scale.data the integrated matrix..")
-  
+
   counts <- counts[,colnames(integrated)]
   data <- CreateSeuratObject(counts = counts, min.features = 0, min.cells = 0,project = "integrated")
   data@meta.data$cluster <- annotation
   data@meta.data$dataset <- dataset
   data <- NormalizeData(object = data)
   VariableFeatures(data) <- rownames(integrated)
-  
+
   data@assays$RNA@scale.data <- integrated
-  
-  
+
+
   data <- RunPCA(data, features = VariableFeatures(object = data))
   plot(ElbowPlot(data))
   data <- FindNeighbors(data, dims = dims)
   data <- FindClusters(data, resolution = res)
   data <- RunUMAP(data, dims = dims)
-  
-  
-  
+
+
+
   DimPlot(object = data, reduction.use = 'umap',group.by = "cluster",no.axes = T,cols = col_anno)+theme_void()+
          theme(legend.text = element_text(size = 14))+ theme(plot.margin = unit(c(0.3,1,1,0), "lines"))
-  
+
   ggsave("plot1.png")
-  
+
   DimPlot(object = data, reduction.use = 'umap',group.by = "dataset",no.axes = T,cols = col_data)+theme_void()+
     theme(legend.text = element_text(size = 14))+ theme(plot.margin = unit(c(0.3,1,1,0), "lines"))
-  
+
   ggsave("plot2.png")
   return(data)
 }
