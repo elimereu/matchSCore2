caf3973b9731d5373f52b413cbff54bcba071185
Federico Marini <marinif@uni-mainz.de>
2020-04-05 01:07:58 +0200

    renamed in mass the files

--- R/ms_identity_map.R
+++ R/ms_identity_map.R
@@ -6,7 +6,7 @@
 #' @param scale.data A scaled matrix of gene expressions like in the `scale.data`
 #' of the Seurat object. Rows are genes and columns are cells from the reference
 #' dataset.
-#' @param model The model as in output of the `train_model` function.
+#' @param model The model as in output of the `ms_train_model` function.
 #' @param gene_cl.ref A list of cluster-specific markers. Each element of the list
 #' contains markers of a cell type. Usually only top100 ranked markers are used.
 #' @param p.threshold Probability threshold to consider a cell classified.
